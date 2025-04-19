0f4a8e799d502496018c8f9a85ee95bc842e1ff0
yuzuyyy <git@stackblitz.com>
2025-04-19 17:53:30 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script> 
+
+</script>
+<div class="padding-x bg-primary">
+<h1 class="heading-2">
+  Hello world!
+</h1>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#BEA6E8",
+        bgDark: "#080808",
+        border: "#1A1A1A",
+        textDark: "#080808",
+        textMid: "#6F6F6F",
+        textLight: "#CBCBCB"
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        alfa: ['Alfa Slab One', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
