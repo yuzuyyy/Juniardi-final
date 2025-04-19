ab39ae0140013ec2e53040b5f482ce5e08524535
yuzuyyy <hendriawan0720@gmail.com>
2025-04-19 18:05:41 +0800

    commit

---
+++ src/assets/index.js
@@ -0,0 +1,44 @@
+import svelte from './svelte.svg'
+import hero from './hero.jpeg'
+import socmed1 from './socmed1.svg'
+import socmed2 from './socmed2.svg'
+import socmed3 from './socmed3.svg'
+import socmed4 from './socmed4.svg'
+import socmed5 from './socmed5.svg'
+import project1 from './project1.jpeg'
+import project2 from './project2.jpeg'
+import project3 from './project3.jpeg'
+import project4 from './project4.jpeg'
+import menu from './menu.svg'
+import close from './close.svg'
+import showcase1 from './showcase1.png'
+import showcase2 from './showcase2.png'
+import showcase3 from './showcase3.png'
+import showcase4 from './showcase4.png'
+import showcase5 from './showcase5.png'
+import showcase6 from './showcase6.png'
+import highlight1 from './highlight1.png'
+import highlight2 from './highlight2.png'
+export {
+    svelte,
+    hero,
+    socmed1,
+    socmed2,
+    socmed3,
+    socmed4,
+    socmed5,
+    project1,
+    project2,
+    project3,
+    project4,
+    menu,
+    close,
+    showcase1,
+    showcase2,
+    showcase3,
+    showcase4,
+    showcase5,
+    showcase6,
+    highlight1,
+    highlight2
+}
---
+++ src/constants/index.js
@@ -0,0 +1,93 @@
+import { project1, project2, project3,  socmed1, socmed2, socmed3, socmed4, socmed5,  } from "../assets";
+
+export const socmeds = [
+    {
+        icon: socmed1,
+        link: "https://twitter.com"
+    },
+    {
+        icon: socmed2,
+        link: "https://instagram.com"
+    },
+    {
+        icon: socmed3,
+        link: "https://dribbble.com"
+    },
+    {
+        icon: socmed4,
+        link: "https://behance.net"
+    },
+    {
+        icon: socmed5,
+        link: "https://linkedin.com"
+    },
+]
+
+export const stats = [
+    {
+        number: "20",
+        simbol: "+",
+        text: "clients"
+    },
+    {
+        number: "45",
+        simbol: "+",
+        text: "projects"
+    },
+    {
+        number: "100",
+        simbol: "%",
+        text: "clients satisfaction"
+    },
+    
+]
+
+export const services = [
+    "Product design",
+    "User Research",
+    "Concept Development",
+    "User Experience Design"
+]
+
+
+
+export const projects = [
+    {
+        image: project1,
+        title: "Z-Watch",
+        description: "Product design"
+    },
+    {
+        image: project2,
+        title: "Bloom Parfume",
+        description: "Product design"
+    },
+    {
+        image: project3,
+        title: "Bottl ecraft",
+        description: "product design"
+    },
+    {
+        image: project1,
+        title: "Z-Watch",
+        description: "Product design"
+    },
+    {
+        image: project2,
+        title: "Bloom Parfume",
+        description: "Product design"
+    },
+    {
+        image: project3,
+        title: "Bottl ecraft",
+        description: "product design"
+    },
+];
+
+
+export const navItems = [
+    { text: "home", link: "#home" },
+    { text: "about", link: "#about" },
+    { text: "project", link: "#project" },
+    { text: "footer", link: "#footer" },
+];
